dcf7e5ca14ace9355634ae4a0346f60fba035954
Tango992 <daniel.rahmanto@gmail.com>
2023-10-24 12:05:44 +0700

    formatting update

--- week-2/day-2/ungraded-5/middleware/middleware.go
+++ week-2/day-2/ungraded-5/middleware/middleware.go
@@ -9,10 +9,12 @@ import (
 
 func Logger(c *gin.Context) {
 	now := time.Now()
+
 	date := now.Format("2006/01/02")
 	time := now.Format("15:04:05")
 	method := c.Request.Method
 	path := c.Request.URL.Path
+	
 	fmt.Printf("%v - %v HTTP request sent to %v%v\n", date, time, method, path)
 	c.Next()
 }
