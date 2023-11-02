942ab3695ca2324d57c2ccc9d47e040dcb495049
Tango992 <daniel.rahmanto@gmail.com>
2023-11-02 16:42:55 +0700

    excercises on websocket

---
+++ week-3/day-4/websocket-demo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+    templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+    return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("./views/*.html")),
+	}
+	e.Renderer = t
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
